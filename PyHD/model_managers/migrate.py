008957a98232f30f75b7c062ba61f2f36a8e3866
Nichita Svisciuc <57572149+saddsuppreme@users.noreply.github.com>
2022-07-06 11:53:11 +0300

    Update migrate.py

--- PyHD/model_managers/migrate.py
+++ PyHD/model_managers/migrate.py
@@ -1,7 +1,7 @@
 """ Here all migrations on database are aplied """
 import sys
-sys.path.append('C:/projects/PyHDproject/PyHD-Web-Framework/PyHD/model_managers')
-from migration_not_needed_class_names import packages
+sys.path.append('C:/projects/PyHDproject/PyHD-Web-Framework/')
+from PyHD.model_managers.migration_not_needed_class_names import packages
 
 import models
 import inspect
@@ -16,32 +16,47 @@ clsmembers = inspect.getmembers(models, inspect.isclass)
 classes = [x for x in clsmembers if x[0] not in packages]
 
 
-def create_column(column_class_object) -> None:
-	db.create_tables([column_class_object], safe=True)
+def create_tables(column_class_object: list) -> None:
+	db.create_tables(column_class_object, safe=True)
 
 
-def print_all_tables(db) -> None:
-	tables = db.get_tables()
-
-	if len(tables) != 0:
-		print('\nAll current tables : ')
-		tables = [''.join(x).capitalize() for x in tables]
-		print(', '.join(tables))
+def print_tables(tables: list) -> None:
+	""" Prints all current tables """
+	if len(tables) > 0:
+		curent_tables = [x.capitalize() for x in tables]
+		print(', '.join(curent_tables))
 	else:
-		print('\nYou dont have any table yet')
+		print('You dont have any tables created yet')
 
 
 if __name__ == '__main__':
 	# Makes tables in SQL Database
-	with models.db as db:
-		open(DATABASE, "w").close()
-		tables = db.get_tables()
+	try:
+		with DATABASE as db:
 
-		# Updates and creates tables
-		for klass in classes:
+			tables = db.get_tables()
 
-			if not klass[0].lower() in tables:
-				create_column(klass[1])
-				print(f'- Table created/updated for {klass[0]}')
+			# Updates and creates tables
+			for klass in classes:
 
-		print_all_tables(db)
+				if not klass[0].lower() in tables:
+					create_tables([klass[1]])
+					print(f'- Table created for {klass[0]}')
+				else:
+					db.drop_tables([klass[1]])
+					create_tables([klass[1]])
+					print(f'- Table {klass[0]} updated')
+
+			# Deletes a table if script detects that it removed from models.py
+			class_names_lower = [x[0].lower() for x in classes]
+
+			for table in tables:
+				if table not in class_names_lower:
+					db.execute_sql(f'DROP TABLE {table}')
+					print(f'- Table {table.capitalize()} deleted')
+
+			# If u want to see all your current tables decoment function below,
+			# or use 'print_tables' anywhere you need
+			""" print_tables(db.get_tables()) """
+	except ValueError:
+		print('Set your database')
